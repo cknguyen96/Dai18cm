95e6740431330a40c0abd4e97dc6bfadc7f472cd
TienHung <hunga13@gmail.com>
2016-05-12 19:38:50 +0700

    no message

---
+++ SpermNoiLoan2/src/com/Dai18cm/GameWindow.java
@@ -0,0 +1,167 @@
+package com.Dai18cm;
+
+import com.Dai18cm.controllers.CollisionPool;
+import com.Dai18cm.controllers.PlayerController;
+import com.Dai18cm.controllers.PlayerDirection;
+import com.Dai18cm.controllers.SpermControllerManager;
+import com.Dai18cm.models.GameConfig;
+import com.Dai18cm.models.Player;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Admin on 5/12/2016.
+ */
+public class GameWindow extends Frame implements Runnable{
+
+    Image backgroundImage;
+    Thread thread;
+    Image backbufferImage;
+    Image image_lv1;
+    PlayerController playerController;
+
+    public GameWindow(){
+        this.playerController = PlayerController.getInst();
+
+        this.setVisible(true);
+        this.setSize(GameConfig.DEFAULT_SCREEN_WIDTH, GameConfig.DEFAULT_SCREEN_HEIGHT);
+
+        try {
+            backgroundImage = ImageIO.read(new File("resources/background.png"));
+            image_lv1 = ImageIO.read(new File("resources/huy_3.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+        });
+
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                PlayerDirection playerDirection = PlayerDirection.NONE;
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        playerDirection = PlayerDirection.LEFT;
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        playerDirection = PlayerDirection.RIGHT;
+                        break;
+                }
+
+                playerController.move(playerDirection);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                PlayerDirection playerDirection = PlayerDirection.NONE;
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                    case KeyEvent.VK_RIGHT:
+                        playerDirection = PlayerDirection.STOP_X;
+                        break;
+                }
+                playerController.move(playerDirection);
+            }
+        });
+
+        thread = new Thread(this);
+        thread.start();
+
+    }
+
+    @Override
+    public void update(Graphics g) {
+        //super.update(g);
+        if (backbufferImage == null){
+            backbufferImage = new BufferedImage(GameConfig.DEFAULT_SCREEN_WIDTH,
+                    GameConfig.DEFAULT_SCREEN_HEIGHT,
+                    1
+            );
+        }
+        Graphics backbuffeGraphics = backbufferImage.getGraphics();
+
+        backbuffeGraphics.drawImage(backgroundImage, 0, 0,
+                GameConfig.DEFAULT_SCREEN_WIDTH,
+                GameConfig.DEFAULT_SCREEN_HEIGHT,
+                null
+        );
+
+        backbuffeGraphics.drawImage(image_lv1,GameConfig.DEFAULT_SCREEN_WIDTH/2, 0,
+                GameConfig.DEFAULT_SCREEN_WIDTH/2,
+                GameConfig.DEFAULT_SCREEN_HEIGHT,
+                null
+        );
+
+        SpermControllerManager.getInst().paint(backbuffeGraphics);
+        playerController.paint(backbuffeGraphics);
+
+        g.drawImage(backbufferImage, 0, 0,
+                GameConfig.DEFAULT_SCREEN_WIDTH,
+                GameConfig.DEFAULT_SCREEN_HEIGHT,
+                null
+        );
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            try {
+                CollisionPool.getInst().run();
+                playerController.run();
+                SpermControllerManager.getInst().run();
+                repaint();
+                Thread.sleep(GameConfig.DEFAULT_THREAD_DELAY);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ SpermNoiLoan2/src/com/Dai18cm/controllers/PlayerController.java
@@ -0,0 +1,75 @@
+package com.Dai18cm.controllers;
+
+import com.Dai18cm.models.*;
+import com.Dai18cm.views.GameDrawer;
+import com.Dai18cm.views.ImageDrawer;
+
+import java.awt.*;
+
+/**
+ * Created by Admin on 5/12/2016.
+ */
+public class PlayerController extends SingleControllerWithHP implements Colliable { //Singleton
+
+    public final int SPEED = 10;
+
+    public PlayerController(GameObject gameObject, GameDrawer gameDrawer, GameVecto gameVecto) {
+        super(gameObject, gameDrawer, gameVecto);
+        CollisionPool.getInst().add(this);
+    }
+
+    public PlayerController(GameObject gameObject, GameDrawer gameDrawer) {
+        super(gameObject, gameDrawer);
+        CollisionPool.getInst().add(this);
+    }
+
+    public void move(PlayerDirection playerDirection) {
+        switch (playerDirection) {
+            case NONE:
+                break;
+            case LEFT:
+                this.gameVecto.dx = -SPEED;
+                break;
+            case RIGHT:
+                this.gameVecto.dx = SPEED;
+                break;
+            case STOP_X:
+                this.gameVecto.dx = 0;
+                break;
+        }
+
+    }
+
+    //Singleton
+    private static PlayerController inst;
+    public static PlayerController getInst() {
+        PlayerController playerController = null;
+        if (inst == null) {
+            Player player = new Player(200, 500, Player.DEFAULT_WIDTH, Player.DEFAULT_HEIGHT);
+            ImageDrawer imageDrawer = new ImageDrawer("resources/Condom.png");
+            playerController = new PlayerController(player, imageDrawer);
+        }
+        return playerController;
+    }
+
+    @Override
+    public void run() {
+        Rectangle rectangle = this.gameObject.getNextRect(this.gameVecto);
+        if(GameConfig.getInst().isInScreen(rectangle) && this.getGameObject().isAlive() == true) {
+            super.run();
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        if(Player.getHP() > 0) super.paint(g);
+    }
+
+    @Override
+    public void onCollide(Colliable c) {
+        if (c instanceof SpermController){
+            Sperm sperm = (Sperm) c.getGameObject();
+            sperm.setAlive(false);
+        }
+    }
+}
